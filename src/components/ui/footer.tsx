354681bf64fada06a39cd00469df296cf2c208be
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-10-18 14:43:14 -0300

    feat: add Footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return ( 
+    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 text-center">
+      © 2023 Copyright <span className="font-semibold">Petro Store</span>
+    </div>
+   );
+}
+ 
+export default Footer;
