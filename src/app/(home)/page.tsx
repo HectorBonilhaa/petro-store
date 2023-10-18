35eb4ea2d3b2382fd8f4692454e6ccdfcb7c9b21
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-10-18 10:15:06 -0300

    feat: add first discount banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+
+export default function Home() {
+  const {data} = useSession();
+  return (
+    <div className="p-5">
+
+      <Image
+        src="/Banner-home-01.png"
+        height={0}
+        width={0}
+        alt="até 55% de desconto esse mês"
+        className="h-auto w-full"
+        sizes="100vw"
+      />
+      
+    </div>
+
+  )
+}
