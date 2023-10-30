de87f197fc26db32bb360dd749fe814e40fbc34b
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-10-30 16:52:10 -0300

    feat: add create order server action

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -9,14 +9,27 @@ import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
 import { createCheckout } from "@/actions/checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { createOrder } from "@/actions/order";
+import { useSession } from "next-auth/react";
 
 const Cart = () => {
+  const { data } = useSession();
+
   const { products, subTotal, total, totalDiscount } = useContext(CartContext);
 
   const handleFinishPurchaseClick = async () => {
-    const checkout = await createCheckout(products);
+    if (!data?.user) {
+      // TODO: redirecionar para o login
+      return;
+    }
+
+    const order = await createOrder(products, (data?.user as any).id);
+
+    const checkout = await createCheckout(products, order.id);
 
     const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+    // Criar pedido no banco
 
     stripe?.redirectToCheckout({
       sessionId: checkout.id,
@@ -25,15 +38,12 @@ const Cart = () => {
 
   return (
     <div className="flex h-full flex-col gap-8">
-      <Badge
-        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
-        variant="outline"
-      >
+      <Badge variant="heading">
         <ShoppingCartIcon size={16} />
         Carrinho
       </Badge>
 
-      {/* Renderizar os produtos */}
+      {/* RENDERIZAR OS PRODUTOS */}
       <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
         <ScrollArea className="h-full">
           <div className="flex h-full flex-col gap-8">
@@ -45,8 +55,8 @@ const Cart = () => {
                 />
               ))
             ) : (
-              <p className="text-left font-semibold">
-                Você ainda não possui produtos em seu carrinho...
+              <p className="text-center font-semibold">
+                Carrinho vazio. Vamos fazer compras?
               </p>
             )}
           </div>
@@ -57,29 +67,29 @@ const Cart = () => {
         <div className="flex flex-col gap-3">
           <Separator />
 
-          <div className="flex items-center justify-between text-xs">
-            <p>Subtotal:</p>
+          <div className="flex items-center justify-between text-xs lg:text-sm">
+            <p>Subtotal</p>
             <p>R$ {subTotal.toFixed(2)}</p>
           </div>
 
           <Separator />
 
-          <div className="flex items-center justify-between text-xs">
-            <p>Entrega:</p>
+          <div className="flex items-center justify-between text-xs lg:text-sm">
+            <p>Entrega</p>
             <p>GRÁTIS</p>
           </div>
 
           <Separator />
 
-          <div className="flex items-center justify-between text-xs">
-            <p>Descontos:</p>
+          <div className="flex items-center justify-between text-xs lg:text-sm">
+            <p>Descontos</p>
             <p>- R$ {totalDiscount.toFixed(2)}</p>
           </div>
 
           <Separator />
 
-          <div className="flex items-center justify-between text-sm font-bold">
-            <p>Total:</p>
+          <div className="flex items-center justify-between text-sm font-bold lg:text-base">
+            <p>Total</p>
             <p>R$ {total.toFixed(2)}</p>
           </div>
 
@@ -87,7 +97,7 @@ const Cart = () => {
             className="mt-7 font-bold uppercase"
             onClick={handleFinishPurchaseClick}
           >
-            Finalizar Compra
+            Finalizar compra
           </Button>
         </div>
       )}
