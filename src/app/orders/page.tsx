3346b070b0be759ec66da1f31b79eac4e99470d4
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-10-30 18:36:12 -0300

    feat: ensure orders are show on orders page

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,40 @@
+import { Badge } from "@/components/ui/badge";
+import { authOptions } from "@/lib/auth";
+import { prismaClient } from "@/lib/prisma";
+import { PackageSearchIcon } from "lucide-react";
+import { getServerSession } from "next-auth";
+import OrderItem from "./components/order-item";
+
+async function OrderPage() {
+  const user = getServerSession(authOptions);
+
+  if (!user) {
+    return <p>Access Denied</p>;
+  }
+
+  const orders = await prismaClient.order.findMany({
+    where: {
+      userId: (user as any).id,
+    },
+    include: {
+      orderProducts: true,
+    }
+  });
+
+  return (
+    <div className="p-5">
+      <Badge variant="heading">
+        <PackageSearchIcon size={16} />
+        Meus Pedidos
+      </Badge>
+
+      <div className="flex flex-col gap-5 mt-3">
+        {orders.map((order) => (
+          <OrderItem key={order.id} order={order} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default OrderPage;
