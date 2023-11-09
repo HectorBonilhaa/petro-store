918f6717d7982564fa6c59c6970563cd216d1b27
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-11-09 09:06:22 -0300

    feat: responsive style desktop in product-info component

--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -8,7 +8,7 @@ interface ProductListProps {
 
 const ProductList = ({ products }: ProductListProps) => {
   return (
-    <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden lg:pb-12 lg:gap-5">
+    <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden lg:pb-12 lg:gap-5 lg:mb-12">
       {products.map((product) => (
         <div key={product.id} className=" w-[170px] max-w-[170px] lg:w-[11.25rem] lg:max-w-[11.25rem] lg:h-[11.875rem] lg:max-h-[11.875rem] lg:gap-5 lg:mb-5">
           <ProductItem product={computeProductTotalPrice(product)} />
