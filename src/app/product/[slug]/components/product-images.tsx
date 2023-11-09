eedd829beea73d342195d8294d7c6aa35664c351
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-11-09 09:06:55 -0300

    feat: responsive style Desktop in product-image component

--- src/app/product/[slug]/components/product-images.tsx
+++ src/app/product/[slug]/components/product-images.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import { useState } from "react";
@@ -13,43 +13,71 @@ const ProductImages = ({ imageUrls, name }: ProductImagesProps) => {
 
   const handleImageClick = (imageUrl: string) => {
     setCurrentImage(imageUrl);
-  }
+  };
 
   return (
-    <div className="flex flex-col">
-      <div className="flex h-[380px] w-full items-center justify-center bg-accent">
+    <div className="flex flex-col gap-12 lg:ml-12 lg:mt-12 lg:w-[46rem] lg:flex-row">
+      <div className="flex h-[380px] w-full bg-accent lg:h-[41.875rem] lg:w-[46rem]">
+        <button className="sm: ml-10 mt-10 hidden h-[77px] w-[80px] flex-col gap-4 lg:flex">
+          {imageUrls.map((imageUrl) => (
+            <div
+              key={imageUrl}
+              className={`flex h-[90px] w-[77px] items-center justify-center gap-4 rounded-lg bg-[#0B0B0B]
+            ${
+              imageUrl === currentImage &&
+              "border-2 border-solid border-primary"
+            }
+          `}
+              onClick={() => handleImageClick(imageUrl)}
+            >
+              <Image
+                src={imageUrl}
+                alt={name}
+                height={0}
+                width={0}
+                sizes="100vw"
+                className="h-auto max-h-[70%] w-auto max-w-[80%]"
+              />
+            </div>
+          ))}
+        </button>
         <Image
           alt={name}
           src={currentImage}
           height={0}
           width={0}
           sizes="100vw"
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          className="mt-20 h-auto max-h-[70%] w-full lg:ml-0 lg:mt-28 lg:h-[24.0625rem] lg:w-[32.125rem]"
           style={{
             objectFit: "contain",
           }}
         />
       </div>
+
       {/* botoes */}
-      <button className="grid grid-cols-4 gap-4 mt-8 px-5">
-      {imageUrls.map((imageUrl) => (
-        <div
-          key={imageUrl}
-          className={`flex h-[90px] items-center justify-center rounded-lg bg-accent
-            ${imageUrl === currentImage && 'border-2 border-solid border-primary'}
+
+      <button className="mt-8 grid grid-cols-4 gap-4 px-5 lg:hidden">
+        {imageUrls.map((imageUrl) => (
+          <div
+            key={imageUrl}
+            className={`flex h-[90px] flex-col items-center justify-center rounded-lg bg-accent
+            ${
+              imageUrl === currentImage &&
+              "border-2 border-solid border-primary"
+            }
           `}
-          onClick={() => handleImageClick(imageUrl)}
+            onClick={() => handleImageClick(imageUrl)}
           >
-          <Image
-            src={imageUrl}
-            alt={name}
-            height={0}
-            width={0}
-            sizes="100vw"
-            className="h-auto max-h-[70%] w-auto max-w-[80%]"
-          />
-        </div>
-      ))}
+            <Image
+              src={imageUrl}
+              alt={name}
+              height={0}
+              width={0}
+              sizes="100vw"
+              className="h-auto max-h-[70%] w-auto max-w-[80%]"
+            />
+          </div>
+        ))}
       </button>
     </div>
   );
